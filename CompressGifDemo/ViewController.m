f57f0dda54a461ea560742bb12ea3116676d28cf
ydd <yuedd@ibobei.cn>
2019-05-17 16:48:48 +0800

    压缩gif

--- CompressGifDemo/ViewController.m
+++ CompressGifDemo/ViewController.m
@@ -8,7 +8,18 @@
 
 #import "ViewController.h"
 
+#import "UIImage+ScallGif.h"
+
+#import "UIImageView+PlayGIF.h"
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *orignSize;
+
+@property (weak, nonatomic) IBOutlet UILabel *compressSize;
+@property (weak, nonatomic) IBOutlet UIImageView *orignImageView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *compressImageView;
 
 @end
 
@@ -16,7 +27,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+// Do any additional setup after loading the view.
+    
+    
+    NSData *gifData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"source2" ofType:@".gif"]];
+    self.orignSize.text = [NSString stringWithFormat:@"%.2fKb", gifData.length / 1024.0];
+    
+    self.orignImageView.gifData = gifData;
+    
+    
+    CGSize imageSize = [UIImage imageNamed:@"source2.gif"].size;
+    
+    NSData *compressData = [UIImage scallGIFWithData:gifData scallSize:CGSizeMake(imageSize.width * 0.5, imageSize.height * 0.5)];
+    self.compressSize.text = [NSString stringWithFormat:@"%.2fKb", compressData.length / 1024.0];
+    
+    self.compressImageView.gifData = compressData;
+    
+    
+}
+
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.orignImageView startGIF];
+    [self.compressImageView startGIF];
 }
 
 
